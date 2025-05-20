6d5f32136c60645c9ed68b77e1ffbbf92038b235
DenisKinyua <kimenjudenis@gmail.com>
2025-05-20 05:18:59 +0300

    Updated configs (redis & docker compose) and creation of Redis config

--- pmsmodule/src/main/java/com/pms/pmsmodule/Configs/Redis/RedisConfig.java
+++ pmsmodule/src/main/java/com/pms/pmsmodule/Configs/Redis/RedisConfig.java
@@ -1,4 +1,34 @@
 package com.pms.pmsmodule.Configs.Redis;
 
+import com.pms.pmsmodule.DTO.PatientRequestDT0;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new Jackson2JsonRedisSerializer<>(PatientRequestDT0.class))
+                );
+        return RedisCacheManager
+                .builder(connectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
 }
