ca4d50e20cf452d85cb5060ba69468d36b63eead
DenisKinyua <kimenjudenis@gmail.com>
2025-05-22 23:33:05 +0300

    create billing account (createBillingAccount)

--- Billing-Service/src/main/java/com/pms/billingservice/grpcserver/BillingGrpcService.java
+++ Billing-Service/src/main/java/com/pms/billingservice/grpcserver/BillingGrpcService.java
@@ -1,4 +1,26 @@
 package com.pms.billingservice.grpcserver;
 
-public class BillingGrpcService {
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver){
+        log.info("createBillingAccount request received {}", billingRequest.toString());
+        //Implement any business logic here : e.g. calculations, database connections
+        BillingResponse billingResponse = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+        responseObserver.onNext(billingResponse);
+        responseObserver.onCompleted();
+    }
 }
