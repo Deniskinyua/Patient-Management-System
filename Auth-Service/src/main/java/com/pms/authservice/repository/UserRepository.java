e0aa42a50c7a796b6410bb6fb61e45562bb10cfa
DenisKinyua <kimenjudenis@gmail.com>
2025-06-04 10:25:59 +0300

    Updating User repository

--- Auth-Service/src/main/java/com/pms/authservice/repository/UserRepository.java
+++ Auth-Service/src/main/java/com/pms/authservice/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.pms.authservice.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.pms.authservice.model.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
