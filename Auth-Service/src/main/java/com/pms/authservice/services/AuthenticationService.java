484e2788c5c212aa169b2e8ef7b7a411617a7888
DenisKinyua <kimenjudenis@gmail.com>
2025-06-04 10:25:24 +0300

    Updating Authentication service and user service

--- Auth-Service/src/main/java/com/pms/authservice/services/AuthenticationService.java
+++ Auth-Service/src/main/java/com/pms/authservice/services/AuthenticationService.java
@@ -1,4 +1,28 @@
 package com.pms.authservice.services;
 
+import com.pms.authservice.dto.LoginRequestDTO;
+import com.pms.authservice.utils.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class AuthenticationService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public Optional<String> authenticateUser(LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(p ->passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        p.getPassword()))
+                .map(u ->jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
+    }
 }
--- Auth-Service/src/main/java/com/pms/authservice/services/UserService.java
+++ Auth-Service/src/main/java/com/pms/authservice/services/UserService.java
@@ -1,4 +1,21 @@
 package com.pms.authservice.services;
 
+import com.pms.authservice.model.User;
+import com.pms.authservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    public Optional<User> findByEmail(String email){
+        //Impelement any business logic here
+
+        return userRepository.findByEmail(email);
+
+    }
 }
