740e6a836c9d55efabbb85b6b826866cca93e1f1
DenisKinyua <kimenjudenis@gmail.com>
2025-06-04 09:39:35 +0300

    Adding authentication controller

--- Auth-Service/src/main/java/com/pms/authservice/controller/AuthenticationController.java
+++ Auth-Service/src/main/java/com/pms/authservice/controller/AuthenticationController.java
@@ -1,4 +1,27 @@
 package com.pms.authservice.controller;
 
+import com.pms.authservice.dto.LoginRequestDTO;
+import com.pms.authservice.dto.LoginResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
 public class AuthenticationController {
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO){
+        Optional<String> token = authenticationService.authenticateUser(loginRequestDTO);
+        if(token.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String convertedToken = token.get();
+        return ResponseEntity.ok(new LoginResponseDTO(convertedToken));
+    }
 }
