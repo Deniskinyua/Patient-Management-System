6f7dfa87642a8c84474f92655f5cd08a79d170c5
DenisKinyua <kimenjudenis@gmail.com>
2025-06-26 23:15:03 +0300

    Adding JwtUtil/validateToken method

--- Auth-Service/src/main/java/com/pms/authservice/utils/JwtUtil.java
+++ Auth-Service/src/main/java/com/pms/authservice/utils/JwtUtil.java
@@ -1,10 +1,13 @@
 package com.pms.authservice.utils;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
@@ -62,5 +65,18 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String jwtToken) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(jwtToken);
+        } catch (SignatureException signatureException){
+            throw new JwtException("Invalid JWT Signature");
+        } catch (JwtException jwtException){
+            throw  new JwtException("Invalid JWT Token");
+        }
+
     }
 }
